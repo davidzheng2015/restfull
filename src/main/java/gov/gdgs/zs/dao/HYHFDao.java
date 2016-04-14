58c25b1ba0250ca660dae79c2308a7b6b4d894a3
tenrock6873155 <1076576310@qq.com>
2016-04-14 10:02:03 +0800

    #101

--- src/main/java/gov/gdgs/zs/dao/HYHFDao.java
+++ src/main/java/gov/gdgs/zs/dao/HYHFDao.java
@@ -12,7 +12,8 @@ import javax.annotation.Resource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import com.gdky.restfull.framework.dao.BaseJdbcDao;
+import com.gdky.restfull.dao.BaseJdbcDao;
+
 
 @Repository
 public class HYHFDao extends BaseJdbcDao{
@@ -21,9 +22,9 @@ public class HYHFDao extends BaseJdbcDao{
 	 public Map<String,Object> hyhf(int pn,int ps){
   	   StringBuffer sb= new StringBuffer();
   	 sb.append("	select  ");
-  	 sb.append("  a.ID AS 'key',a.ID,a.ND,a.JGNAME,CONCAT(a.ZYYWSR,'元') AS ZYYWSR,CONCAT(a.ZJNE,'元') AS ZJNE ,");	 
-  	  sb.append(" CONCAT(a.YJTTHF,'元') AS YJTTHF,CONCAT(a.YJTTHF1,'元') AS YJTTHF1,a.QJTTHF,CONCAT(a.YJGRHF,'元') AS YJGRHF,");
-  	 sb.append(" CONCAT(a.YJGRHF2,'元') AS YJGRHF2,a.QJGRHF,a.JG_ID");
+  	 sb.append("  a.ID AS 'key',a.ID,a.ND,a.JGNAME,FORMAT(a.ZYYWSR,2) AS ZYYWSR,FORMAT(a.ZJNE,2) AS ZJNE,");	 
+  	  sb.append(" FORMAT(a.YJTTHF,2) AS YJTTHF,FORMAT(a.YJTTHF1,2) AS YJTTHF1,a.QJTTHF,FORMAT(a.YJGRHF,2) AS YJGRHF,");
+  	 sb.append(" FORMAT(a.YJGRHF2,2) AS YJGRHF2,a.QJGRHF,a.JG_ID");
   	 sb.append(" FROM zs_hyhf a");
  
   	   List<Map<String,Object>> ls= this.jdbcTemplate.queryForList(sb.toString());
@@ -39,9 +40,9 @@ public class HYHFDao extends BaseJdbcDao{
 	
 	 public Map<String,Object> hyhf1(){
 	   		StringBuffer sb = new StringBuffer();
-	   	   sb.append(" SELECT a.ID AS 'key',a.ID,a.ND,a.JGNAME,CONCAT(a.ZYYWSR,'元') AS ZYYWSR,CONCAT(a.ZJNE,'元') AS ZJNE ,");	 
-	   	  sb.append(" CONCAT(a.YJTTHF,'元') AS YJTTHF,CONCAT(a.YJTTHF1,'元') AS YJTTHF1,a.QJTTHF,CONCAT(a.YJGRHF,'元') AS YJGRHF,");
-	   	 sb.append(" CONCAT(a.YJGRHF2,'元') AS YJGRHF2,a.QJGRHF,a.JG_ID");
+	   	   sb.append(" SELECT a.ID AS 'key',a.ID,a.ND,a.JGNAME,FORMAT(a.ZYYWSR,2) AS ZYYWSR,FORMAT(a.ZJNE,2) AS ZJNE,");	 
+	   	  sb.append(" FORMAT(a.YJTTHF,2) AS YJTTHF,FORMAT(a.YJTTHF1,2) AS YJTTHF1,a.QJTTHF,FORMAT(a.YJGRHF,2) AS YJGRHF,");
+	   	 sb.append(" FORMAT(a.YJGRHF2,2) AS YJGRHF2,a.QJGRHF,a.JG_ID");
 	   	 sb.append(" FROM zs_hyhf a");
 		   List<Map<String,Object>> f1= this.jdbcTemplate.queryForList(sb.toString());
 	   		Map<String,Object> ob = new HashMap<>();
@@ -51,7 +52,7 @@ public class HYHFDao extends BaseJdbcDao{
 	 
 	 public Map<String,Object> grhf1(int pn,int ps){
 	  	   StringBuffer sb= new StringBuffer();
-	  	 sb.append(" SELECT a.JG_ID AS 'key',a.JG_ID AS xh,a.ID,a.ND,b.MC AS DQ,a.JGNAME,a.SEX,DATE_FORMAT(a.SQDATE,'%Y-%m-%d') AS JFSJ");
+	  	 sb.append(" SELECT a.JG_ID AS 'key',a.JG_ID AS xh,a.ID,a.ND,b.MC AS DQ,a.JGNAME,FORMAT(a.SEX,2) AS SEX,DATE_FORMAT(a.SQDATE,'%Y-%m-%d') AS JFSJ");
 	  	 sb.append("  FROM zs_grhf a,dm_cs b");	 
 	  	  sb.append(" WHERE a.DQ=b.ID AND a.TYPE=1  ORDER BY a.ND DESC,ID DESC");	 
 	  	   List<Map<String,Object>> ls= this.jdbcTemplate.queryForList(sb.toString());
@@ -65,7 +66,7 @@ public class HYHFDao extends BaseJdbcDao{
 	    }
 	 public Map<String,Object> grhf(){
 	   		StringBuffer sb = new StringBuffer();
-	   	 sb.append(" SELECT a.JG_ID AS 'key',a.JG_ID AS xh,a.ID,a.ND,b.MC AS DQ,a.JGNAME,a.SEX,DATE_FORMAT(a.SQDATE,'%Y-%m-%d') AS JFSJ");
+	   	 sb.append(" SELECT a.JG_ID AS 'key',a.JG_ID AS xh,a.ID,a.ND,b.MC AS DQ,a.JGNAME,FORMAT(a.SEX,2) AS SEX,DATE_FORMAT(a.SQDATE,'%Y-%m-%d') AS JFSJ");
 	  	 sb.append("  FROM zs_grhf a,dm_cs b");	 
 	  	  sb.append(" WHERE a.DQ=b.ID AND a.TYPE=1  ORDER BY a.ND DESC,ID DESC");
 	  	List<Map<String,Object>> ls = this.jdbcTemplate.queryForList(sb.toString());
@@ -82,7 +83,7 @@ public class HYHFDao extends BaseJdbcDao{
 	 }
 	 public Map<String,Object> xx(int id){
   	   StringBuffer sb = new StringBuffer();
-  	 sb.append(" SELECT a.ID,a.JG_ID,a.ND,b.MC AS DQ,a.JGNAME,a.NAME,a.MONEY,a.SEX,a.SQDATE");
+  	 sb.append(" SELECT a.ID,a.JG_ID,a.ND,b.MC AS DQ,a.JGNAME,a.NAME,CONCAT(a.MONEY,'元') AS MONEY,CONCAT(a.SEX,'元') AS SEX,DATE_FORMAT(a.SQDATE,'%Y-%m-%d %H:%i:%s') AS SQDATE");
  	  sb.append("  FROM zs_grhf a,dm_cs b");	 
  	  sb.append(" WHERE a.TYPE=1 AND a.DQ=b.ID  AND a.ID=? ORDER BY a.ND DESC,ID DESC");	 	    
  	 Map<String,Object> bg = this.jdbcTemplate.queryForMap(sb.toString(),new Object[]{id});
