e194ef2ea9060ac071f5f39180a82d895f674df5
mervyn.chung <mervyn.chung@foxmail.com>
2016-04-18 10:54:21 +0800

    #111

--- src/main/java/gov/gdgs/zs/service/YwglService.java
+++ src/main/java/gov/gdgs/zs/service/YwglService.java
@@ -2,6 +2,7 @@ package gov.gdgs.zs.service;
 
 import gov.gdgs.zs.dao.YwglDao;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,14 +11,30 @@ import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 @Service
 public class YwglService {
 
 	@Resource
 	private YwglDao ywglDao;
 
-	public Map<String, Object> getYwxy(int page, int pageSize) {
-		return  ywglDao.getYwxy(page,pageSize);
+	public Map<String, Object> getYwxy(int page, int pageSize, String where) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		if (where != null) {
+			try {
+				where = java.net.URLDecoder.decode(where, "UTF-8");
+				ObjectMapper mapper = new ObjectMapper();
+				map = mapper.readValue(where,
+						new TypeReference<Map<String, Object>>() {
+						});
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		return ywglDao.getYwxy(page, pageSize,map);
 	}
 
 }
