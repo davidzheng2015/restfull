2616061a93f87a070a10088ca494bc82e013c764
tenrock6873155 <1076576310@qq.com>
2016-04-27 10:19:04 +0800

    33

--- src/main/java/gov/gdgs/zs/api/CWBBController.java
+++ src/main/java/gov/gdgs/zs/api/CWBBController.java
@@ -9,34 +9,33 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.RestController;
 
-import gov.gdgs.zs.configuration.ProjectConstants;
+import gov.gdgs.zs.configuration.Config;
 import gov.gdgs.zs.dao.CWBBDao;
 import gov.gdgs.zs.service.CWBBService;
 
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.HttpStatus;
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController 
-// @RequestMapping(value = ProjectConstants.URL_PROJECT) 
+@RequestMapping(value = Config.URL_PROJECT)
 
 public class CWBBController {
 	
 	@Resource 
-//	 	private CWBBService cwbbService; 
+	 	private CWBBService cwbbService; 
         private CWBBDao cwbbDao;
-//	@RequestMapping(value = "/xygl", method = RequestMethod.GET) 
-//	 	public  ResponseEntity<Map<String,Object>> getAsideMenu( 
-//	 			@RequestParam(value = "page", required = true) int page, 
-//	 			@RequestParam(value = "pageSize", required = true) int pageSize, 
-//	 			@RequestParam(value="where", required=false) String where){ 
-//	 		Map<String,Object> obj = cwbbService.lrfp(page,pageSize,where); 
-//	 		return new ResponseEntity<>(obj,HttpStatus.OK); 
-//	 	} 
+	@RequestMapping(value = "/zcmx", method = RequestMethod.GET) 
+	 	public  ResponseEntity<Map<String,Object>> zcmx( 
+	 			@RequestParam(value = "page", required = true) int page, 
+	 			@RequestParam(value = "pageSize", required = true) int pageSize, 
+	 			@RequestParam(value="where", required=false) String where){ 
+	 		Map<String,Object> obj = cwbbService.zcmx(page,pageSize,where); 
+	 		return new ResponseEntity<>(obj,HttpStatus.OK); 
+	 	} 
 
 	 @RequestMapping(value="/api/lrfp", method = {RequestMethod.GET})  
 	 public Map<String,Object> lrfp(HttpServletRequest request ) { 
--- src/main/java/gov/gdgs/zs/dao/CWBBDao.java
+++ src/main/java/gov/gdgs/zs/dao/CWBBDao.java
@@ -1,11 +1,12 @@
 package gov.gdgs.zs.dao;
 
+import gov.gdgs.zs.configuration.Config;
 import gov.gdgs.zs.untils.Pager;
+import gov.gdgs.zs.untils.Condition;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Iterator;
 import java.util.Map;
 
 import org.springframework.stereotype.Repository;
@@ -13,39 +14,55 @@ import org.springframework.stereotype.Repository;
 import com.gdky.restfull.dao.BaseJdbcDao;
 @Repository
 public class CWBBDao extends BaseJdbcDao{
-	public Map<String, Object> lrfp(int page, int pageSize, Map<String,Object> where) {
-		StringBuffer condition = new StringBuffer(); 
-		 		if (where.size() != 0) { 
-		 			Iterator<Map.Entry<String, Object>> entries = where.entrySet() 
-		 					.iterator(); 
-		 			while (entries.hasNext()) { 
-		 				Map.Entry<String, Object> entry = entries.next(); 
-		 				condition.append(" and "); 
-		 				condition.append(entry.getKey() + " like :" + entry.getKey() + " "); 
-		 			} 
-		 		} 
+	public Map<String, Object> zcmx(int page, int pageSize, Map<String,Object> where) {
+		
+		Condition condition = new Condition();
+		condition.add("b.DWMC", "FUZZY", where.get("DWMC"));
+		
 
-		
-		int startIndex = pageSize * (page-1) ;
+
 		StringBuffer sb = new StringBuffer();
-		sb.append("	SELECT SQL_CALC_FOUND_ROWS b.ID AS 'key',b.id,b.DWMC,DATE_FORMAT(a.JSSJ,'%Y-%m-%d') AS TJSJ,a.DWFZR,");
-		sb.append(" a.CKFZR,CASE a.ZTBJ WHEN 0 THEN \"保存\"  WHEN 1 THEN \"提交\" ELSE NULL END AS ZTBJ");
-		sb.append(" FROM zs_lrfp a,zs_jg b");
-		sb.append(" WHERE a.JG_ID=b.ID AND a.ZTBJ=1  ORDER BY a.JSSJ");
-		sb.append("		    LIMIT ?, ? ");
-		int total = jdbcTemplate.queryForObject("SELECT FOUND_ROWS()", Integer.class);
-		List<Map<String,Object>> ls = jdbcTemplate.queryForList(sb.toString(),new Object[]{startIndex,pageSize});
+		sb.append(" SELECT  SQL_CALC_FOUND_ROWS @rownum:=@rownum+1 AS 'key',t.*");
+		sb.append(" FROM (select");
+		sb.append(" a.id,");
+		sb.append(" b.DWMC,");
+		sb.append(" a.ZYYWCB,");
+		sb.append(" a.ZYYWSJFJ,a.QTYWZC,a.GLFY,a.CWFY,");
+		sb.append(" CASE a.ZTBJ WHEN 0 THEN '保存' WHEN 1 THEN '提交' ELSE NULL END AS ZTBJ,");
+		sb.append(" DATE_FORMAT(a.JSSJ,'%Y-%m-%d') AS TJSJ");
+		sb.append(" FROM "+Config.PROJECT_SCHEMA+"zs_cwbb_zcmx a,zs_jg b,(SELECT @rownum:=?) temp");
+		sb.append(condition.getSql());//相当元 where b.DWMC like '%%'
+		sb.append(" AND a.JG_ID=b.ID AND a.ZTBJ=1) as t");
+		sb.append("    LIMIT ?, ? ");
+		// 装嵌传值数组
+		int startIndex = pageSize * (page - 1);
+		ArrayList<Object> params = condition.getParams();
+		params.add(0,pageSize * (page - 1));
+		params.add(startIndex);
+		params.add(pageSize);
 		
-		Map<String,Object> obj = new HashMap<String,Object>();
+
+		// 获取符合条件的记录
+		List<Map<String, Object>> ls = jdbcTemplate.queryForList(sb.toString(),
+				params.toArray());
+
+		// 获取符合条件的记录数
+		
+		int total = jdbcTemplate.queryForObject("SELECT FOUND_ROWS()",  Integer.class);
+
+		Map<String, Object> obj = new HashMap<String, Object>();
 		obj.put("data", ls);
 		obj.put("total", total);
-		return obj;
+		obj.put("pageSize", pageSize);
+		obj.put("current", page);
+
+				return obj;
 	}
 	public Map<String,Object> lrfp1(int pn,int ps){
 	  	   StringBuffer sb= new StringBuffer();
 	  	 sb.append("	SELECT  b.ID AS 'key',a.id,a.JG_ID,b.DWMC,DATE_FORMAT(a.JSSJ,'%Y-%m-%d') AS TJSJ,a.DWFZR,");
 			sb.append(" a.CKFZR,CASE a.ZTBJ WHEN 0 THEN \"保存\"  WHEN 1 THEN \"提交\" ELSE NULL END AS ZTBJ");
-			sb.append(" FROM zs_lrfp a,zs_jg b");
+			sb.append(" FROM "+Config.PROJECT_SCHEMA+"zs_cwbb_lrfp a,zs_jg b");
 			sb.append(" WHERE a.JG_ID=b.ID AND a.ZTBJ=1  ORDER BY a.JSSJ");	 
 	  	   List<Map<String,Object>> ls= this.jdbcTemplate.queryForList(sb.toString());
 	  	   Pager<Map<String,Object>> pager =Pager.create(ls,ps);
@@ -60,7 +77,7 @@ public class CWBBDao extends BaseJdbcDao{
 	   		StringBuffer sb = new StringBuffer();
 	   		sb.append("	SELECT  b.ID AS 'key',a.id,a.JG_ID,b.DWMC,DATE_FORMAT(a.JSSJ,'%Y-%m-%d') AS TJSJ,a.DWFZR,");
 			sb.append(" a.CKFZR,CASE a.ZTBJ WHEN 0 THEN \"保存\"  WHEN 1 THEN \"提交\" ELSE NULL END AS ZTBJ");
-			sb.append(" FROM zs_lrfp a,zs_jg b");
+			sb.append(" FROM "+Config.PROJECT_SCHEMA+"zs_cwbb_lrfp a,zs_jg b");
 			sb.append(" WHERE a.JG_ID=b.ID AND a.ZTBJ=1  ORDER BY a.JSSJ");
 	  	List<Map<String,Object>> ls = this.jdbcTemplate.queryForList(sb.toString());
 		List<Map<String,Object>> fl = new ArrayList<Map<String,Object>>();
@@ -80,7 +97,7 @@ public class CWBBDao extends BaseJdbcDao{
 	  sb.append("  a.KFPLR,a.KFPLRUPYEAR,a.YYGJ,a.YYGJUPYEAR,a.JLFLJJ,a.JLFLJJUPYEAR,a.CBJJ,a.CBJJUPYEAR,a.QYFZJJ,a.QYFZJJUPYEAR,");	 
 	  sb.append(" a.LRGHTZ,a.LRGHTZUPYEAR,a.TZZFPLR,a.TZZFPLRUPYEAR,a.YXGL,a.YXGLUPYEAR,a.PTGL,a.PTGLUPYEAR,a.ZHPTGL,a.ZHPTGLUPYEAR,");
 	  sb.append(" a.WFPLR,a.WFPLRUPYEAR,a.DWFZR,a.CKFZR,a.FHR,a.ZBR");	
-	  sb.append(" FROM zs_lrfp a,zs_jg b");	
+	  sb.append(" FROM "+Config.PROJECT_SCHEMA+"zs_cwbb_lrfp a,zs_jg b");	
 	  sb.append(" WHERE a.JG_ID=b.ID AND a.ZTBJ=1 AND a.id=? ORDER BY a.JSSJ");	
 	 Map<String,Object> bg = this.jdbcTemplate.queryForMap(sb.toString(),new Object[]{id});
 	   Map<String,Object> ob = new HashMap<>();
