e75b248fa4146b6d96694a6cba849caa43451f18
mervyn.chung <mervyn.chung@foxmail.com>
2016-05-10 00:30:52 +0800

    #12

--- src/main/java/com/gdky/restfull/dao/UserDao.java
+++ src/main/java/com/gdky/restfull/dao/UserDao.java
@@ -4,8 +4,10 @@ import javax.annotation.Resource;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
+@Transactional
 public class UserDao {
 	@Resource(name ="jdbcTemplate")
 	private JdbcTemplate jdbcTemplate;
