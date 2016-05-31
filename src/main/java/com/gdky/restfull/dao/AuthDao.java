89b452b62bdd0882312357ba99213d361a58de6d
mervyn.chung <mervyn.chung@foxmail.com>
2016-05-31 17:29:01 +0800

    #2

---
+++ src/main/java/com/gdky/restfull/dao/AuthDao.java
@@ -0,0 +1,16 @@
+package com.gdky.restfull.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.gdky.restfull.entity.Role;
+
+@Repository
+public class AuthDao extends BaseJdbcDao{
+	
+	public List<Role> getRoles (){
+		return null;
+	}
+
+}
